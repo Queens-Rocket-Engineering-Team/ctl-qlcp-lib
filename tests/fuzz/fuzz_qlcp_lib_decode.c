82e38a6ad9d04f2ee1c9fb11a1f470a40f9cc9f0
NathanBelza <nathanbelza1@gmail.com>
2026-06-16 15:10:57 -0400

    fix missing files for fuzzing

---
+++ tests/fuzz/fuzz_qlcp_lib_decode.c
@@ -0,0 +1,56 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "qlcp_lib.h"
+
+static qlcp_sensor_data g_sensor_buffer[200];
+static qlcp_control_data g_control_buffer[200];
+static uint8_t g_config_buffer[2048];
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+
+    memset(g_sensor_buffer, 0, sizeof(g_sensor_buffer));
+    memset(g_control_buffer, 0, sizeof(g_control_buffer));
+    memset(g_config_buffer, 0, sizeof(g_config_buffer));
+    
+    // decode helper fuzzing
+    size_t magic_num_idx = 0;
+    qlcp_find_magic_num(&magic_num_idx, data, size);
+
+    uint16_t packet_len = 0;
+    qlcp_get_packet_len(&packet_len, data, size);
+
+    // fix buffer with magic number and version
+    if (size < QLCP_HEADER_SIZE) {
+        return 0;
+    }
+    if (size > 4096) {
+        size = 4096;
+    }
+    uint8_t fixed_data[4096];
+    memcpy(fixed_data, data, size);
+    static const uint8_t qlcp_magic_num[] = {'Q', 'L', 'C', 'P'};
+    static const uint8_t qlcp_version_num = 3;
+    memcpy(fixed_data, qlcp_magic_num, 4);
+    fixed_data[4] = qlcp_version_num;
+
+    // client decode fuzzing
+    qlcp_client_payload client_payload = {0};
+    qlcp_decode_server_to_client(&client_payload, fixed_data, size);
+
+    // server decode fuzzing
+    qlcp_server_payload server_payload = {0};
+
+    qlcp_server_payload_buffers payload_buffers = {
+        .sensor_data = g_sensor_buffer,
+        .sensor_data_len = sizeof(g_sensor_buffer)/sizeof(g_sensor_buffer[0]),
+        .control_data = g_control_buffer,
+        .control_data_len = sizeof(g_control_buffer)/sizeof(g_control_buffer[0]),
+        .config_data = g_config_buffer,
+        .config_data_len = sizeof(g_config_buffer)
+    };
+
+    qlcp_decode_client_to_server(&server_payload, &payload_buffers, fixed_data, size);
+
+    return 0; 
+}
